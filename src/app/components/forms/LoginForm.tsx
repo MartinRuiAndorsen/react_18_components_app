3f86a07c6062c627ca7512cbc0e0c09a80f28d11
Martin Andorsen <martin.andorsen@inmeta.no>
2024-04-17 15:58:11 +0200

    Start work on centering LoginFrom

--- src/app/components/forms/LoginForm.tsx
+++ src/app/components/forms/LoginForm.tsx
@@ -3,11 +3,15 @@ import styles from "./LoginForm.module.css";
 export default function LoginForm() {
   return (
     <form className={styles.form}>
-      <label>Email</label>
-      <input name="test input" />
-      <label typeof="password">Password</label>
-      <input type="password" />
-      <button type="submit">Submit</button>
+      <div>
+        <label>Email</label>
+        <input name="test input" />
+      </div>
+      <div>
+        <label typeof="password">Password</label>
+        <input type="password" />
+        <button type="submit">Submit</button>
+      </div>
     </form>
   );
 }
