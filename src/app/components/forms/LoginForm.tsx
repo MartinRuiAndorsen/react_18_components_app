c65b7a29ef718801372f638f1f3267482d7aa3bc
Martin Andorsen <martin.andorsen@inmeta.no>
2024-04-19 15:32:34 +0200

    style login form

--- src/app/components/forms/LoginForm.tsx
+++ src/app/components/forms/LoginForm.tsx
@@ -3,15 +3,9 @@ import styles from "./LoginForm.module.css";
 export default function LoginForm() {
   return (
     <form className={styles.form}>
-      <div>
-        <label>Email</label>
-        <input name="test input" />
-      </div>
-      <div>
-        <label typeof="password">Password</label>
-        <input type="password" />
-        <button type="submit">Submit</button>
-      </div>
+      <input type="email" placeholder="Email" />
+      <input type="password" placeholder="Password" />
+      <button type="submit">Login</button>
     </form>
   );
 }
